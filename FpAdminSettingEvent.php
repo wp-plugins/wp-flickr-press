989427d80f2bea7436ab0b6d46329f2e9fd61a54
tatsuya <tatsuya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-22 01:58:04 +0000

    release 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-flickr-press/tags/1.2.0@399646 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ FpAdminSettingEvent.php
@@ -0,0 +1,194 @@
+<?php
+class FpAdminSettingEvent {
+	private function __construct() {}
+
+	public static function addMenu() {
+		$page = add_options_page(FlickrPress::NAME.' Option', FlickrPress::NAME, 8, __FILE__, array('FpAdminSettingEvent','generateOptionForm'));
+	}
+
+	public static function addWhitelistOptions($whitelist_options) {
+		$whitelist_options['wpfp'] = array(
+            FlickrPress::getKey('api_key'),
+            FlickrPress::getKey('api_secret'),
+            FlickrPress::getKey('user_id'),
+            FlickrPress::getKey('username'),
+            FlickrPress::getKey('oauth_token'),
+            FlickrPress::getKey('default_target'),
+            FlickrPress::getKey('default_align'),
+            FlickrPress::getKey('default_size'),
+            FlickrPress::getKey('insert_template'),
+            FlickrPress::getKey('default_sort'),
+            FlickrPress::getKey('quick_settings'),
+            FlickrPress::getKey('default_search_type'),
+		);
+		return $whitelist_options;
+	}
+
+	public static function generateOptionForm() {
+	        $alignes = array(
+                'None'=>'none',
+                'Left'=>'left',
+                'Center'=>'center',
+                'Right'=>'right',
+	        );
+	        $targets = array(
+                'None'=>'',
+                'New Window'=>'_blank',
+        	);
+	        $sorts = array(
+                'Posted ASC' => 'date-posted-asc',
+                'Posted DESC' => 'date-posted-desc',
+                'Taken ASC' => 'date-taken-asc',
+                'Taken DESC' => 'date-taken-desc',
+                'Interestingness ASC' => 'interestingness-asc',
+                'Interestingness DESC' => 'interestingness-desc',
+	        );
+	        $searchTypes = array(
+	        	'List' => 'list',
+	        	'Thumbnail' => 'thumbnail',
+	        );
+?>
+<script type="text/javascript"><!--
+(function($){
+	$(function(){
+		$('#fp-oauth-token-btn').bind('click', function(){
+			window.open('<?php echo esc_url(FlickrPress::getPluginUrl().'/flickr_oauth.php') ?>', 'flikcr_oauth', 'width=800,height=600,menubar=no, toolbar=no, scrollbars=yes');
+		});
+	});
+})(jQuery);
+function callback_oauth(token) {
+	jQuery('#fp-oauth-token').html(token.token);
+	jQuery('#fp-oauth-token-hid').val(token.token);
+	jQuery('#fp-user-id').html(token.user.nsid);
+	jQuery('#fp-user-id-hid').val(token.user.nsid);
+	jQuery('#fp-username').html(token.user.username);
+	jQuery('#fp-username-hid').val(token.user.username);
+	jQuery('#fp-oauth-update').html("<?php __('Last Update: ', FlickrPress::TEXT_DOMAIN) ?>"+new Date()+"<br/><p style=\"color:#f00\"><strong>Not yet been update. Please Update.</strong><p>");
+}
+
+// --></script>
+<div class="wrap">
+        <h2><?php echo FlickrPress::NAME ?></h2>
+        <form method="post" action="options.php">
+                <?php wp_nonce_field('wpfp-options'); ?>
+                <input type="hidden" name="action" value="update" />
+                <input type="hidden" name="option_page" value="wpfp" />
+                <table class="form-table">
+                        <tr valign="top">
+                                <th scope="row">
+                                        <p><?php echo __('API KEY/Secret', FlickrPress::TEXT_DOMAIN) ?></p>
+					<p><a href="http://www.flickr.com/services/api/misc.api_keys.html" target="_blank">Flickr Services</a></p>
+                                </th>
+                                <td>
+					<p><?php echo __('API KEY:', FlickrPress::TEXT_DOMAIN) ?><br/><input type="text" name="<?php echo FlickrPress::getKey('api_key') ?>" value="<?php echo FlickrPress::getApiKey() ?>" size="70" />
+					<p><?php echo __('Secret:', FlickrPress::TEXT_DOMAIN) ?><br/><input type="text" name="<?php echo FlickrPress::getKey('api_secret') ?>" value="<?php echo FlickrPress::getApiSecret() ?>" size="70" />
+				</td>
+                        </tr>
+                        <tr valign="top">
+                                <th scope="row">
+                                        <p><?php echo __('OAuth Token', FlickrPress::TEXT_DOMAIN) ?></p>
+                                </th>
+                                <td>
+					<input id="fp-oauth-token-hid" type="hidden" name="<?php echo FlickrPress::getKey('oauth_token') ?>" value="<?php echo FlickrPress::getOAuthToken() ?>" />
+					<input id="fp-user-id-hid" type="hidden" name="<?php echo FlickrPress::getKey('user_id') ?>" value="<?php echo FlickrPress::getUserId() ?>" />
+					<input id="fp-username-hid" type="hidden" name="<?php echo FlickrPress::getKey('username') ?>" value="<?php echo FlickrPress::getUsername() ?>" />
+					<p>UserID: <span id="fp-user-id"><?php echo FlickrPress::getUserId() ?></span></p>
+					<p>Username: <span id="fp-username"><?php echo FlickrPress::getUsername() ?></span></p>
+					<p>Token: <span id="fp-oauth-token"><?php echo FlickrPress::getOAuthToken() ?></span></p>
+					<p><a href="javascript:void(0)" class="button" id="fp-oauth-token-btn"><?php __('Update OAuth Token', FlickrPress::TEXT_DOMAIN) ?></a></p>
+					<p id="fp-oauth-update"><p>
+				</td>
+                        </tr>
+
+                </table>
+
+		<h3><?php echo __('Default Attachments', FlickrPress::TEXT_DOMAIN) ?></h3>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><p><?php echo __('Link Target', FlickrPress::TEXT_DOMAIN) ?></p></th>
+				<td>
+					<?php foreach($targets as $label => $target) { ?>
+						<?php $checked = FlickrPress::getDefaultTarget()==$target ? " checked='checked'" : '' ?>
+						<input type="radio" name="<?php echo FlickrPress::getKey('default_target') ?>" id="target-<?php echo $target ?>" value="<?php echo $target ?>" <?php echo $checked ?>/><label for="target-<?php echo $target ?>"><?php echo $label ?></label>
+					<?php } ?>
+				</td>
+			</tr>
+                        <tr valign="top">
+                                <th scope="row"><p><?php echo __('Alignment', FlickrPress::TEXT_DOMAIN) ?></p></th>
+                                <td>
+                                        <?php foreach($alignes as $label => $align) { ?>
+						<?php $checked = FlickrPress::getDefaultAlign()==$align ? " checked='checked'" : '' ?>
+                                                <input type="radio" name="<?php echo FlickrPress::getKey('default_align') ?>" id="alignment-<?php echo $align ?>" value="<?php echo $align ?>" <?php echo $checked ?>/><label for="alignment-<?php echo $align ?>"><?php echo $label ?></label>
+                                        <?php } ?>
+                                </td>
+                        </tr>
+                        <tr valign="top">
+                                <th scope="row"><p><?php echo __('Size', FlickrPress::TEXT_DOMAIN) ?></p></th>
+                                <td>
+									<?php foreach(FlickrPress::$SIZE_LABELS as $size => $label) { ?>
+										<?php $checked = FlickrPress::getDefaultSize()==$size ? " checked='checked'" : '' ?>
+                                            <p><input type="radio" name="<?php echo FlickrPress::getKey('default_size') ?>" id="size-<?php echo $size ?>" value="<?php echo $size ?>" <?php echo $checked ?>/><label for="size-<?php echo $size ?>"><?php echo $label ?></label></p>
+									<?php } ?>
+                                </td>
+                        </tr>
+		</table>
+		
+		<h3><?php echo __('Advanced Options', FlickrPress::TEXT_DOMAIN) ?></h3>
+		<table class="form-table">
+                        <tr valign="top">
+                                <th scope="row">
+					<p><?php echo __('Insert Template', FlickrPress::TEXT_DOMAIN) ?></p>
+					<h4><?php echo __('Avalable Options', FlickrPress::TEXT_DOMAIN) ?></h4>
+					<p>[img]: Image Tag</p>
+					<p>[title]: Image Title</p>
+				</th>
+                                <td>
+					<textarea name="<?php echo FlickrPress::getKey('insert_template') ?>" cols="70" rows="10"><?php echo FlickrPress::getInsertTemplate() ?></textarea>
+					<p>If you put a newline at the beginning or end, &lt;br/&gt; Please write tags.</p>
+					<p>However, &lt;br/&gt; if there is a line break before and after the tag, I been wrapped them too.</p>
+                                </td>
+                        </tr>
+                        <tr valign="top">
+                                <th scope="row">
+					<p><?php echo __('Default Sort', FlickrPress::TEXT_DOMAIN) ?></p>
+				</th>
+                                <td>
+					<select name="<?php echo FlickrPress::getKey('default_sort') ?>">
+					<?php foreach ($sorts as $label => $sort) { ?>
+						<option value="<?php echo $sort ?>" <?php if ($sort==FlickrPress::getDefaultSort()) {echo "selected='selected'";} ?>><?php echo $label ?></option>
+					<?php } ?>
+					</select>
+                                </td>
+                        </tr>
+			<tr valign="top">
+				<th scope="row">
+					<p><?php echo __('Quick Settings', FlickrPress::TEXT_DOMAIN) ?></p>
+				</th>
+				<td>
+					<p><?php echo __('Enable:', FlickrPress::TEXT_DOMAIN) ?><input type="checkbox" name="<?php echo flickrpress::getkey('quick_settings') ?>" value="1" <?php if (FlickrPress::getQuickSettings()=='1') { echo "checked='checked'"; } ?>/></p>
+					<p>When enabled, a dialog will appear when you click for a set of check boxes for multiple insertion.</p>
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">
+					<p><?php echo __('Search Type', FlickrPress::TEXT_DOMAIN) ?></p>
+				</th>
+				<td>
+					<p>
+					<?php foreach ($searchTypes as $label => $val) { ?>
+						<input type="radio" name="<?php echo FlickrPress::getKey('default_search_type') ?>" id="search_type_<?php echo $val ?>" value="<?php echo $val ?>" <?php echo FlickrPress::getDefaulSearchType()==$val ? 'checked="checked"' : ''; ?>/><label for="search_type_<?php echo $val ?>"><?php echo __($label, FlickrPress::TEXT_DOMAIN) ?></label>
+					<?php } ?>
+					</p>
+				</td>
+			</tr>
+		</table>
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php __('Save Changes', FlickrPress::TEXT_DOMAIN) ?>" />
+		</p>
+	</form>
+</div>
+
+<?php
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,105 @@
+=== wp-flickr-press ===
+Contributors: tatsuya
+Donate link: http://fukata.org/
+Tags: images,flickr
+Requires at least: 3.0.1
+Tested up to: 3.1.3
+Stable tag: 1.2.0
+
+Flickr画像を記事本文内に挿入する。
+
+== Description ==
+
+当プラグインは、投稿画面よりFlickrの画像を記事本文内に簡単に挿入するためのプラグインです。
+
+最新のソースは、下記より取得できます。
+http://github.com/fukata/wp-flickr-press/
+
+PHP依存ライブラリ
+
+1. php-curl http://php.net/manual/ja/book.curl.php
+
+== Installation ==
+
+1. 解凍後、フォルダ「wp-flickr-press/」をディレクトリ「/wp-content/plugins/」にアップロードする。
+2. 同ディレクトリ内のcacheディレクトリに対し、書き込み権限を付与する。 
+3. 管理画面よりプラグイン「FlickrPress」をアクティベートを行う。
+4. 左サイドバーに表示されているFlickrPressの設定画面より、APIキー、ユーザIDについて設定する。
+
+== Frequently Asked Questions ==
+
+現在なし
+
+== Screenshots ==
+
+1. Add flickr media 1
+2. Add flickr media 2
+3. Tag suggest
+4. Batch insert into post
+5. Quick Settings
+6. New UI Search
+7. New UI Insert Post
+8. Setting config
+
+== Changelog ==
+== 1.2.0 =
+* 国際化(日本語)対応
+
+ 1.1.1 =
+* jqueryのflickrプラグイン内のconsole.logをコメントアウト
+
+= 1.1.0 =
+* 旧UIの速度改善
+
+= 1.0.0 =
+* 新UIを実装
+
+= 0.9.0 =
+* POSTで呼び出していた部分をGETに変更
+
+= 0.8.0 =
+* 画像個別に取得していたサイズ情報を取得しないように修正
+
+= 0.7.1 =
+* 特権管理者以外でも更新できるように修正
+
+= 0.7.0 =
+* クイック設定を追加
+
+= 0.6.0 =
+* 挿入順序を追加
+
+= 0.5.0 =
+* デフォルトのソート項目を追加
+
+= 0.4.0 =
+* 検索項目にソートを追加
+
+= 0.3.0 =
+* ユーザIDの取得方法を直入力からOAuth連携に修正
+* 画像挿入時のテンプレートを追加
+
+= 0.2.0 =
+* photosetで検索した際に詳細URL内のownerが抜けていたバグを修正
+* マルチサイトを適用した場合に、ページングが動作していなかったバグを修正
+
+= 0.1.0 =
+* 一括挿入機能を追加
+
+= 0.0.7 =
+* タグサジェスト機能を追加
+
+= 0.0.6 =
+* Photosetsのプルダウンが選択状態が維持されないバグを修正
+* キャッシュクリア用のボタンを追加
+
+= 0.0.5 =
+* API KEY、API SECRET、USER IDを未設定チェックを追加
+
+= 0.0.4 =
+* 検索機能追加
+
+= 0.0.1 =
+* 初回リリース
+
+== Upgrade Notice ==
