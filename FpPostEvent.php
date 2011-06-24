a65b444286d15f990ce5dcb8159eada7d57e682e
tatsuya <tatsuya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-24 05:35:41 +0000

    release 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-flickr-press/tags/1.3.0@400713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ FpPostEvent.php
@@ -0,0 +1,33 @@
+<?php
+require_once(dirname(__FILE__).'/FlickrPress.php');
+
+class FpPostEvent {
+	private function __construct() {
+	}
+
+	public static function addButtons() {
+		$context = __('Upload/Insert %s', FlickrPress::TEXT_DOMAIN);
+		$context .= self::_media_button(__('Add flickr media', FlickrPress::TEXT_DOMAIN), FlickrPress::getPluginUrl().'/images/icon-flickr.gif', FlickrPress::MEDIA_BUTTON_TYPE);
+		return $context;
+	}
+	
+	public static function loadScripts() {
+		$html = '';
+		$html = '<script src="'.FlickrPress::getPluginUrl().'/js/media_upload.js'.'" type="text/javascript"></script>'."\n";
+		echo $html;
+	}
+
+	public static function getUploadIframeSrc($uploadIframeSrc) {
+		if (FlickrPress::getDefaulSearchType() == 'thumbnail') {
+			return FlickrPress::getPluginUrl().'/'.$uploadIframeSrc.'&mode=search_thumbnail';
+		} else {
+			return FlickrPress::getPluginUrl().'/'.$uploadIframeSrc.'&mode=search';
+		}
+	}
+
+	private static function _media_button($title, $icon, $type) {
+	        return "<a href='" . esc_url( get_upload_iframe_src($type) ) . "' id='add_$type' class='thickbox' title='$title'><img src='" . esc_url( $icon ) . "' alt='$title' /></a>";
+	}
+}
+
+?>
