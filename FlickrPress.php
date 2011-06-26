96e0303dca696650940ae49016b5726ac444b7c2
tatsuya <tatsuya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-26 06:15:57 +0000

    release 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-flickr-press/tags/1.3.1@401352 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ FlickrPress.php
@@ -0,0 +1,161 @@
+<?php
+require_once(dirname(__FILE__).'/libs/phpflickr/phpFlickr.php');
+
+class FlickrPress {
+	// constants	
+	const NAME = 'FlickrPress';
+	const PREFIX = 'wpfp_';
+	const MEDIA_BUTTON_TYPE = 'flickr_media';
+	const TEXT_DOMAIN = 'wp-flickr-press';
+
+	private static $flickr;
+	
+	public static $SIZE_LABELS = array(
+		'sq' => 'Square',
+		't' => 'Thumbnail',
+		's' => 'Small',
+		'm' => 'Medium',
+		'o' => 'Original',
+	);
+	public static $SIZES = array(
+		'sq' => 'url_sq',
+		't' => 'url_t',
+		's' => 'url_s',
+		'm' => 'url_m',
+		'o' => 'url_o',
+	);
+	private function __construct() {
+	}
+
+	public static function init() {
+		self::loadLanguages();
+		self::addEvents();
+
+		self::$flickr = new phpFlickr(FlickrPress::getApiKey(), FlickrPress::getApiSecret());
+		if (self::isCacheEnabled()) {
+			self::$flickr->enableCache(FlickrPress::getCacheType(), FlickrPress::getCacheConnection());
+		}
+	}
+
+	public static function clearCache() {
+		$cacheDir = self::getCacheConnection();
+		if (strlen($cacheDir)==0 || !is_writeable($cacheDir)) {
+			return;
+		}
+
+		$dir = dir($cacheDir);
+		while ( ($file=$dir->read()) !== false ) {
+			if (preg_match('/^.*\.cache$/', $file)) {
+				unlink("{$cacheDir}{$file}");
+			}
+		}
+	}
+
+	public static function isCacheEnabled() {
+		return true;
+	}
+
+	public static function getClient() {
+		return self::$flickr;
+	}
+
+	public static function getDir() {
+		return dirname(__FILE__);
+	}
+
+	public static function getCacheType() {
+		return 'fs';
+	}
+
+	public static function getCacheConnection() {
+		return dirname(__FILE__).'/cache/';
+	}
+
+	public static function getApiKey() {
+		return get_option(self::getKey('api_key'));
+	}
+
+        public static function getApiSecret() {
+                return get_option(self::getKey('api_secret'));
+        }
+
+	public static function getUserId() {
+		return get_option(self::getKey('user_id'));
+	}
+
+	public static function getUsername() {
+		return get_option(self::getKey('username'));
+	}
+
+	public static function getOAuthToken() {
+		return get_option(self::getKey('oauth_token'));
+	}
+
+	public static function getPluginUrl() {
+		return plugins_url('wp-flickr-press');
+	}
+
+	public static function getDefaultTarget() {
+		return get_option(self::getKey('default_target'), '');
+	}
+
+	public static function getDefaultAlign() {
+		return get_option(self::getKey('default_align'), 'none');
+	}
+
+	public static function getDefaultSize() {
+		return get_option(self::getKey('default_size'), 'Medium');
+	}
+
+	public static function getDefaulSearchType() {
+		return get_option(self::getKey('default_search_type'), 'list');
+	}
+
+	public static function getInsertTemplate() {
+		return get_option(self::getKey('insert_template'), '[img]');
+	}
+
+	public static function getDefaultSort() {
+		return get_option(self::getKey('default_sort'), 'date-posted-desc');
+	}
+
+	public static function getQuickSettings() {
+		return get_option(self::getKey('quick_settings'), 0);
+	}
+
+	public static function getKey($key) {
+		return self::PREFIX . $key;
+	}
+
+	private static function addEvents() {
+		// load action or filter
+		require_once(self::getDir().'/FpPostEvent.php');
+		add_action('media_buttons_context', array('FpPostEvent', 'addButtons'));
+		add_filter(self::MEDIA_BUTTON_TYPE.'_upload_iframe_src', array('FpPostEvent', 'getUploadIframeSrc'));
+		add_action('admin_head-post-new.php', array('FpPostEvent', 'loadScripts'));
+
+		require_once(self::getDir().'/FpAdminSettingEvent.php');
+		add_action('admin_menu', array('FpAdminSettingEvent', 'addMenu'));
+		add_filter('whitelist_options', array('FpAdminSettingEvent', 'addWhitelistOptions'));
+	}
+
+	public static function getPhotoUrl($photo, $size='m') {
+		return $photo[self::$SIZES[$size]];
+	}
+
+	public static function getPhotoPageUrl($photo, $photos) {
+		$id = $photo['id'];
+		$owner = isset($photo['owner']) ? $photo['owner'] : false;
+		if (!$owner && isset($photos['owner'])) {
+			$owner = $photos['owner'];
+		}
+
+		$url = "http://www.flickr.com/photos/$owner/$id";
+		return $url;
+	}
+	
+	public static function loadLanguages() {
+		load_plugin_textdomain(self::TEXT_DOMAIN, false, 'wp-flickr-press/languages');
+	}
+}
+?>
---
+++ check-setting.php
@@ -0,0 +1,21 @@
+<?php
+$errors = array();
+$apiKey = FlickrPress::getApiKey();
+$apiSecret = FlickrPress::getApiSecret();
+if (empty($apiKey) || empty($apiSecret)) {
+	$errors[] = __('API KEY and API SECRET is required. Please setting API KEY and API SECRET.', FlickrPress::TEXT_DOMAIN);
+}
+
+$userId = FlickrPress::getUserId();
+if (empty($userId)) {
+	$errors[] = __('USER ID is required. Please setting USER ID.', FlickrPress::TEXT_DOMAIN);
+}
+
+if (!empty($errors) && count($errors)>0) {
+	$buffer = "";
+	foreach ($errors as $error) {
+		$buffer .= "<p>{$error}</p>";
+	}
+	wp_die($buffer);
+}
+?>
