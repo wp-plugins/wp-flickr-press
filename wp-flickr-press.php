2133f94beab82c15d36487ba30a2852c4ae7e7f6
tatsuya <tatsuya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-28 12:04:15 +0000

    release 1.7.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-flickr-press/tags/1.7.1@468547 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-flickr-press.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: wp-flickr-press
+Plugin URI: http://fukata.org/dev/wp-plugin/wp-flickr-press/
+Description: Flickr integration for wordpress plugin.
+Version: 1.7.1
+Author: Tatsuya Fukata
+Author URI: http://fukata.org
+*/
+
+require_once(dirname(__FILE__).'/FlickrPress.php');
+
+FlickrPress::init();
+?>
