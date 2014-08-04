c0c53cee5f334924786071d9cb5f4090768fe033
tatsuya <tatsuya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 10:46:03 +0000

    release 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-flickr-press/tags/2.0.0@959883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr_oauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once(dirname(__FILE__).'/check-oauth.php');
-
-FlickrPress::getClient()->auth("delete");
-$token = FlickrPress::getClient()->auth_checkToken();
-include_once(dirname(__FILE__).'/inc.flickr_oauth_callback.php');
-?>
--- inc.flickr_oauth_callback.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript"><!--
-window.opener.callback_oauth(<?php echo json_encode($token) ?>);
-window.close();
-// --></script>
